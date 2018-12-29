7b07a1a525d6396d041d503aa31ab122542e1cb5
石涛 <u6584092@anu.edu.au>
2018-12-29 11:04:09 +0800

    Initial commit

---
+++ venv/MergeSortTest.py
@@ -0,0 +1,30 @@
+def mergesort(unsorted):
+    if len(unsorted) <= 1:
+        return unsorted
+    size = len(unsorted)/2
+    left_unsorted = mergesort(unsorted[:size])
+    right_unsorted = mergesort(unsorted[size:])
+    left = 0
+    right = 0
+    result = []
+    while left < len(left_unsorted) or right < len(right_unsorted):
+        if left < len(left_unsorted) and (right == len(right_unsorted) or left_unsorted[left]<=right_unsorted[right]):
+            result.append(left_unsorted[left])
+            left += 1
+        else:
+            result.append(right_unsorted[right])
+            right += 1
+    return result
+
+
+print(mergesort([12, -3, 5, 27, 19, 2, 10, 15, 90, 1]))
+
+
+
+
+
+
+
+
+
+
